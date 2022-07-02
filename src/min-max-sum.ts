bbbe8a9779f028f6416186d4d4705b0aa62c41d9
Patrick Coutinho <patrick@istyle.com.br>
2022-07-02 11:13:33 -0300

    test: created min max sum

---
+++ src/min-max-sum.ts
@@ -0,0 +1,12 @@
+export const minMaxSum = (arr: number[]): number[] => {
+  const totalSum = arr.reduce((item, sum) => item + sum);
+  const possibleSums: number[] = [];
+
+  arr.forEach((item, index) => {
+    possibleSums[index] = totalSum - item;
+  });
+
+  possibleSums.sort();
+
+  return [possibleSums[0], possibleSums[possibleSums.length - 1]];
+};

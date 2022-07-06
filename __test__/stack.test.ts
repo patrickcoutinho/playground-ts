c9678f8658453a5eaab3d13af7cd5a3911ff6530
Patrick Coutinho <patrick@istyle.com.br>
2022-07-06 20:43:04 -0300

    test: added test to call pop on empty stack

--- __test__/stack.test.ts
+++ __test__/stack.test.ts
@@ -23,6 +23,15 @@ describe('Stack Tests Playground', () => {
     expect(stack.size()).toBe(1);
   });
 
+  test('should try to remove an element from empty stack and receive undefined', () => {
+    const stack = new Stack();
+
+    const item = stack.pop();
+
+    expect(item).toBeUndefined();
+    expect(stack.size()).toBe(0);
+  });
+
   test('should return the stack size', () => {
     const stack = new Stack();
 

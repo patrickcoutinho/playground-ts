6a9cff175d7dd33da54e0aa1904b59cf2c6b8a14
Patrick Coutinho <patrick@istyle.com.br>
2022-07-07 20:40:29 -0300

    test: created tests for dequeue method

--- __test__/queue.test.ts
+++ __test__/queue.test.ts
@@ -11,6 +11,22 @@ describe('Queue Playground Tests', () => {
     expect(queue.size()).toEqual(1);
   });
 
+  test('should dequeue the first item', () => {
+    const queue = new Queue();
+
+    expect(queue.size()).toEqual(0);
+
+    queue.queueUp(4);
+    queue.queueUp(56);
+
+    expect(queue.size()).toEqual(2);
+
+    const item = queue.dequeue();
+
+    expect(item).toBe(4);
+    expect(queue.size()).toEqual(1);
+  });
+
   test('should return the queue size', () => {
     const queue = new Queue();
 

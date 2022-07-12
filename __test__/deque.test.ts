dc2751ddd457a7459e9d3d76e778b87473f3ef53
Patrick Coutinho <patrick@istyle.com.br>
2022-07-12 20:10:18 -0300

    test: created tests for initial methods

--- __test__/deque.test.ts
+++ __test__/deque.test.ts
@@ -6,4 +6,42 @@ describe('Deque Tests Playground', () => {
 
     expect(deque).toBeInstanceOf(Deque);
   });
+
+  test('should return the Deque size', () => {
+    const deque = new Deque();
+
+    expect(deque.size()).toBe(0);
+
+    deque.addFront(21);
+    deque.addFront(44);
+    deque.addFront(67);
+
+    expect(deque.size()).toBe(3);
+  });
+
+  test('should return if Deque is empty', () => {
+    const deque = new Deque();
+
+    expect(deque.isEmpty()).toBeTruthy();
+
+    deque.addFront(21);
+
+    expect(deque.isEmpty()).toBeFalsy();
+  });
+
+  test('should add an item to front of Deque', () => {
+    const deque = new Deque();
+
+    deque.addFront(21);
+
+    expect(deque.size()).toBe(1);
+  });
+
+  test('should add an item to back of Deque', () => {
+    const deque = new Deque();
+
+    deque.addBack(21);
+
+    expect(deque.size()).toBe(1);
+  });
 });

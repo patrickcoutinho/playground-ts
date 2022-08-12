c5738a923bb465cd69febcd45dcc26ce14b76962
Patrick Coutinho <patrick@istyle.com.br>
2022-08-12 18:43:55 -0300

    test: ensure that LinkedList was created with the correct values

--- __test__/linked-list.test.ts
+++ __test__/linked-list.test.ts
@@ -1,9 +1,20 @@
 import { LinkedList } from '../src/linked-list';
+import { faker } from '@faker-js/faker';
 
 describe('Linked List Tests Playground', () => {
   test('should create a new instance of LinkedList', () => {
-    const linkedList = new LinkedList(10);
+    const linkedList = new LinkedList(faker.random.alphaNumeric());
 
     expect(linkedList).toBeInstanceOf(LinkedList);
+  });
+
+  test('should create a LinkedList with correct values', () => {
+    const headValue = faker.random.alphaNumeric();
+
+    const linkedList = new LinkedList(headValue);
+
+    expect(linkedList.head.value).toBe(headValue);
+    expect(linkedList.head.next).toBe(null);
+    expect(linkedList.length()).toBe(1);
   });
 });

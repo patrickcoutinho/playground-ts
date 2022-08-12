7a93afc0334da51be61113d820fcfffb68bc3bfe
Patrick Coutinho <patrick@istyle.com.br>
2022-08-12 19:09:55 -0300

    feat: created append method

--- src/linked-list.ts
+++ src/linked-list.ts
@@ -7,6 +7,7 @@ export class LinkedList<T> {
   private count: number = 0;
 
   public head: Node<T> | undefined = undefined;
+  public tail: Node<T> | undefined = undefined;
 
   constructor(value: T) {
     this.head = {
@@ -14,10 +15,22 @@ export class LinkedList<T> {
       next: null,
     };
 
+    this.tail = this.head;
     this.count++;
   }
 
   public length() {
     return this.count;
+  }
+
+  public append(value: T) {
+    const newNode = {
+      value,
+      next: null,
+    };
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.count++;
   }
 }

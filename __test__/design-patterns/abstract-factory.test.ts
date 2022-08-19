7973cf1fe5e7a900634106bc5509ec1264511c6f
Patrick Coutinho <patrick@istyle.com.br>
2022-08-19 17:16:05 -0300

    test: abstract factory playground

---
+++ __test__/design-patterns/abstract-factory.test.ts
@@ -0,0 +1,31 @@
+import {
+  clientCode,
+  HouseOneFactory,
+  HouseTwoFactory,
+} from '../../src/design-patterns/abstract-factory';
+
+describe('Abstract Factory', () => {
+  test('HouseOneFactory', () => {
+    const { dog, cat } = clientCode(new HouseOneFactory());
+
+    expect(cat.paws).toEqual(4);
+    expect(dog.paws).toEqual(4);
+
+    expect(cat.sound()).toEqual('miaaauuuu');
+    expect(dog.sound()).toEqual('au au');
+
+    expect(cat.soundTogether(dog)).toEqual('miaaauuuu (au au)');
+  });
+
+  test('HouseTwoFactory', () => {
+    const { dog, cat } = clientCode(new HouseTwoFactory());
+
+    expect(cat.paws).toEqual(4);
+    expect(dog.paws).toEqual(4);
+
+    expect(cat.sound()).toEqual('miau miau');
+    expect(dog.sound()).toEqual('aau aau au au au');
+
+    expect(cat.soundTogether(dog)).toEqual('miau miau + aau aau au au au');
+  });
+});

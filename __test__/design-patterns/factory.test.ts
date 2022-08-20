a0377bf20d1a265407af0c7e266a4073ffcf05eb
Patrick Coutinho <patrick@istyle.com.br>
2022-08-20 12:25:40 -0300

    test: factory playground

---
+++ __test__/design-patterns/factory.test.ts
@@ -0,0 +1,36 @@
+import {
+  clientCode,
+  ConcreteCreator1,
+  ConcreteCreator2,
+  ConcreteProduct1,
+  ConcreteProduct2,
+} from '../../src/design-patterns/factory';
+
+describe('Factory Playground', () => {
+  test('should call operation from ConcreteProduct1', () => {
+    const product = clientCode(new ConcreteCreator1()).create();
+
+    expect(product).toBeInstanceOf(ConcreteProduct1);
+    expect(product.operation()).toEqual('{Result of the ConcreteProduct1}');
+  });
+
+  test('should call operation from ConcreteProduct2', () => {
+    const product = clientCode(new ConcreteCreator2()).create();
+
+    expect(product).toBeInstanceOf(ConcreteProduct2);
+    expect(product.operation()).toEqual('{Result of the ConcreteProduct2}');
+  });
+
+  test('should call someOperation from Creator using correct product', () => {
+    const product1 = clientCode(new ConcreteCreator1());
+    const product2 = clientCode(new ConcreteCreator2());
+
+    expect(product1.someOperation()).toEqual(
+      "Creator: The same creator's code has just worked with {Result of the ConcreteProduct1}",
+    );
+
+    expect(product2.someOperation()).toEqual(
+      "Creator: The same creator's code has just worked with {Result of the ConcreteProduct2}",
+    );
+  });
+});

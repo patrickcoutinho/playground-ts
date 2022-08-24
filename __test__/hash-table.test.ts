9edfd70f24cb04f4dde4c9d831fee0796ce10380
Patrick Coutinho <patrick@istyle.com.br>
2022-08-24 20:34:34 -0300

    test: simple hash table playground

---
+++ __test__/hash-table.test.ts
@@ -0,0 +1,21 @@
+import { faker } from '@faker-js/faker';
+import { HashTable } from '../src/hash-table';
+
+describe('HashTable Playground', () => {
+  test('should create a HashTable', () => {
+    const hashTable = new HashTable(10);
+
+    expect(hashTable).toBeInstanceOf(HashTable);
+  });
+
+  test('should insert and get value from HashTable', () => {
+    const key = faker.random.word();
+    const value = faker.random.alphaNumeric();
+
+    const hashTable = new HashTable(10);
+
+    hashTable.set(key, value);
+
+    expect(hashTable.get(key)).toEqual(value);
+  });
+});

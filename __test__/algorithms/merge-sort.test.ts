163933291c1a93e99f0d97969164e53af0d59145
Patrick Coutinho <patrick@istyle.com.br>
2022-08-31 22:43:54 -0300

    test: created merge sort

---
+++ __test__/algorithms/merge-sort.test.ts
@@ -0,0 +1,10 @@
+import { mergeSort } from '../../src/algorithms/merge-sort';
+
+const unorderedArray = [6, 1, 78, 99, 3, 11, 56, 32, 67];
+const orderedArray = [1, 3, 6, 11, 32, 56, 67, 78, 99];
+
+describe('Merge Sort Playground', () => {
+  test('should sort a unordered array using merge sort', () => {
+    expect(mergeSort(unorderedArray)).toEqual(orderedArray);
+  });
+});
